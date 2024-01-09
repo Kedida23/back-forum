60474063ef5e8efdb7cc2c44ba5de3e4b04d85d9
Kedida23 <147494973+Kedida23@users.noreply.github.com>
2024-01-09 12:58:07 -0500

    dbconfig abedated

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,16 +1,15 @@
 const mysql2 = require("mysql2");
 require("dotenv").config();
 const dbconnection = mysql2.createPool({
-	user: "forum-evangadi",
-	database: "forum-evangadi",
-	host: "localhost",
-	connectionLimit: 10,
-	// user: process.env.USER,
-	// database: process.env.DATABASE,
-	// host: process.env.HOST,
-	// password: process.env.PASSWORD,
-	// connectionLimit: process.env.LIMIT,
+	// user: "forum-evangadi",
+	// database: "forum-evangadi",
+	// host: "localhost",
+	// connectionLimit: 10,
+	user: process.env.USER,
+	database: process.env.DATABASE,
+	host: process.env.HOST,
+	connectionLimit: process.env.LIMIT,
+	password: process.env.PASSWORD,
 });
-
 
 module.exports = dbconnection.promise();
